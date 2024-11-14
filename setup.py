608f9df7fd32607d31bbee52c8de50a78c28b15b
jianfengmao <jianfengmao@deephaven.io>
2024-11-14 14:17:46 -0700

    Update project's Development Status classifier

--- setup.py
+++ setup.py
@@ -371,7 +371,7 @@ setup(name='jpy',
           'install_lib': JpyInstallLib
       },
       python_requires='>=3.6',
-      classifiers=['Development Status :: 4 - Beta',
+      classifiers=['Development Status :: 5 - Production/Stable',
                    # Indicate who your project is intended for
                    'Intended Audience :: Developers',
 
