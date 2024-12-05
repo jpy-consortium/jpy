042d5102d5f56f028225951e8d0cd3d766e5c489
Devin Smith <devinsmith@deephaven.io>
2024-12-05 15:05:28 -0800

    Bump to 1.1.0 development

--- jpyutil.py
+++ jpyutil.py
@@ -36,7 +36,7 @@ import subprocess
 __author__ = "Norman Fomferra (Brockmann Consult GmbH) and contributors"
 __copyright__ = "Copyright 2015-2018 Brockmann Consult GmbH and contributors"
 __license__ = "Apache 2.0"
-__version__ = "1.0.0"
+__version__ = "1.1.0.dev0"
 
 
 # Setup a dedicated logger for jpyutil.
